e378074a3e3c3e979f31bb0f56f4b3ae0be299ba
pravo23 <pravo23@192.168.1.6>
2024-04-02 02:54:44 +0530

    prettfied the code

--- commands/list.js
+++ commands/list.js
@@ -1,25 +1,28 @@
-const conf = new (require('conf'))()
-const chalk = require('chalk');
+const conf = new (require("conf"))();
+const chalk = require("chalk");
 
 function list() {
-    const todoList = conf.get('todo-list');
+  const todoList = conf.get("todo-list");
 
-    if (todoList && todoList.length) {
-        // user have tasks in todo-list
-        console.log(chalk.blue.bold('Task in green are done. task in yellow are still not done.'));
+  if (todoList && todoList.length) {
+    // user have tasks in todo-list
+    console.log(
+      chalk.blue.bold(
+        "Task in green are done. task in yellow are still not done.",
+      ),
+    );
 
-        todoList.forEach((task, index) => {
-            
-            if (task.done) {
-                console.log(chalk.greenBright(`${index + 1}. ${task.text}`));
-            } else {
-                console.log(chalk.yellowBright(`${index + 1}. ${task.text}`));
-            }
-        });
-    } else {
-        // user does not have task in todo-list
-        console.log(chalk.red.bold('You don\'t have any task yet.'));
-    }
+    todoList.forEach((task, index) => {
+      if (task.done) {
+        console.log(chalk.greenBright(`${index + 1}. ${task.text}`));
+      } else {
+        console.log(chalk.yellowBright(`${index + 1}. ${task.text}`));
+      }
+    });
+  } else {
+    // user does not have task in todo-list
+    console.log(chalk.red.bold("You don't have any task yet."));
+  }
 }
 
 module.exports = list;
