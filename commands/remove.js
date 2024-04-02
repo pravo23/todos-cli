6832f97860a91ba9114dc3f0291a9ab8d646d7a2
pravo23 <pravo23@192.168.1.6>
2024-04-02 18:13:10 +0530

    moved the constants to single file

--- commands/remove.js
+++ commands/remove.js
@@ -1,13 +1,14 @@
 const conf = new (require("conf"))();
 const chalk = require("chalk");
+const constants = require('../constants');
 
 const removeTasks = (tasks) => {
-  let todosList = conf.get("todo-list") || [];
-  let filteredTodoLists = todosList.filter(
+  let todoLists = conf.get(constants.todoLists) || [];
+  let filteredTodoLists = todoLists.filter(
     (_, index) => !tasks.includes((index + 1).toString()),
   );
 
-  conf.set("todo-list", filteredTodoLists);
+  conf.set(constants.todoLists, filteredTodoLists);
 
   if (tasks.length > 0) {
     console.log(
